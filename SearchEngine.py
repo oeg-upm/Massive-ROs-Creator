bc8af2797e20ad9c0840621d2c2824cf8dd162a1
George <georgehadib@gmail.com>
2021-10-27 10:37:16 +0200

    Update SearchEngine.py

--- SearchEngine.py
+++ SearchEngine.py
@@ -3,14 +3,14 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
-from html.parser import HTMLParser
 import time
 
 domain = "Natural sciences"
 field = "Earth science"
-subfield = "Geology"
+subfield = ""
+description = "climate"
 
-PATH = r'C:\Users\Geo\Downloads\chromedriver_win32\chromedriver.exe'
+PATH = r'A static path to your local webdriver'
 driver = webdriver.Chrome(PATH)
 link = "https://archive.sigma2.no/pages/public/search.jsf"
 
@@ -18,23 +18,42 @@ driver.get(link)
 
 advanced_search = driver.find_element_by_id("searchForm:j_idt59:header:inactive")
 advanced_search.click()
-driver.get(link)
-domain_menu = Select(driver.find_element_by_id("searchForm:domainMenu"))
-#domain_menu.click()
-domain_menu.select_by_visible_text("Natural sciences")
-time.sleep(0.5)
-field_menu = Select(driver.find_element_by_id("searchForm:fieldMenu"))
-field_menu.select_by_visible_text("Earth science")
-time.sleep(0.5)
-subfield_menu = Select(driver.find_element_by_id("searchForm:subfieldMenu"))
-subfield_menu.select_by_visible_text("Geology")
-time.sleep(0.5)
+time.sleep(1)
+#driver.get(link)
 
+#Add domain
+if (not domain==""):
+	domain_menu = Select(driver.find_element_by_id("searchForm:domainMenu"))
+	domain_menu.select_by_visible_text(domain)
+	time.sleep(0.5)
+
+#Add field
+if (not field==""):
+	field_menu = Select(driver.find_element_by_id("searchForm:fieldMenu"))
+	field_menu.select_by_visible_text(field)
+	time.sleep(0.5)
+
+#Add subfield
+if (not subfield==""):
+	subfield_menu = Select(driver.find_element_by_id("searchForm:subfieldMenu"))
+	subfield_menu.select_by_visible_text(subfield)
+
+#Add description
+if (not description==""):
+	description_input = driver.find_element_by_name("searchForm:j_idt86")
+	description_input.send_keys(description)
+
+#excute query
 search_button = driver.find_element_by_name("searchForm:j_idt318").click()
-content = driver.find_element_by_id("searchresult-section")
-list_of_content = content.find_elements_by_class_name("rf-edt-c-cnt")
-i = 0
-list_of_ids = []
+
+#scrape list
+try:
+	content = driver.find_element_by_id("searchresult-section")
+	list_of_content = content.find_elements_by_class_name("rf-edt-c-cnt")
+	list_of_ids = []
+except:
+	NoSuchElementException: print ("There is no results for your search. Please modify your enteries and try again")
+	exit()
 
 for i in range (0, len(list_of_content),5):
 	list_of_content[i] = list_of_content[i].get_attribute("innerHTML")
@@ -43,7 +62,34 @@ for i in range (0, len(list_of_content),5):
 	list_of_content[i]
 	#print (list_of_content[i])
 	list_of_ids.append(list_of_content[i])
-print (list_of_ids)
+
+page_counter = 2
+
+while (1):
+	time.sleep(1)
+
+	try:
+
+		next_page = driver.find_element_by_id ("searchResultForm:j_idt61_ds_"+str(page_counter)).click()
+		time.sleep(1)
+
+		print("breakpoint 1")
+		content = driver.find_element_by_id("searchresult-section")
+		list_of_content = content.find_elements_by_class_name("rf-edt-c-cnt")
+		for i in range (0, len(list_of_content),5):
+			list_of_content[i] = list_of_content[i].get_attribute("innerHTML")
+
+			list_of_content[i] = list_of_content[i][list_of_content[i].find(""";">""")+3:list_of_content[i].find(""";">""")+22]
+			list_of_content[i]
+			#print (list_of_content[i])
+			list_of_ids.append(list_of_content[i])
+		page_counter+=1
+	except:
+		NoSuchElementException:	print (list_of_ids)
+		print("Your querey was excuted correctly and information was saved")
+		exit()
+
+
 
 #list_of_content[i].find("</a")
 #####################ESTA ES UNA PRUEBA PARA SACAR MÁS DATOS DE LA LISTA######################
@@ -64,4 +110,4 @@ print (list_of_ids)
 
 #domain_menu.send_keys(domain)
 #domain_menu.send_keys(Keys.RETURN)
-driver.quit()
+#driver.quit()
