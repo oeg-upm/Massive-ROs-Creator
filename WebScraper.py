22ce0b42b786896a4f98dbbd27cf5820939d98da
George <georgehadib@gmail.com>
2021-11-06 22:27:28 +0100

    Update WebScraper.py

--- WebScraper.py
+++ WebScraper.py
@@ -1,80 +1,144 @@
-import selenium
 import json
 from selenium import webdriver
-from html.parser import HTMLParser
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+import time
 
-PATH = r'Here goes the absolute location of your webdriver'
+domain = "Natural sciences"
+field = "Earth science"
+subfield = ""
+description = "air"
+categories = ["Experiment", "Observation", "Model", "Simulation", "Software", "Image", "Calibration"]
+
+PATH = r'C:\Users\Geo\Downloads\chromedriver_win32\chromedriver.exe'
 driver = webdriver.Chrome(PATH)
-f = open("Massive-ROs-Creator\ToScrape.json", "r")
-entry_json = f.read()
-f.close()
-entry_dictionary = json.loads(entry_json)
-result_list = []
-for i in range (0, len(entry_dictionary)):
+link = "https://archive.sigma2.no/pages/public/search.jsf"
 
+driver.get(link)
+time.sleep(1)
+advanced_search = driver.find_element_by_id("searchForm:j_idt59:header:inactive")
+advanced_search.click()
+time.sleep(1)
+#driver.get(link)
+#Add domain
+if (not domain==""):
+	domain_menu = Select(driver.find_element_by_id("searchForm:domainMenu"))
+	domain_menu.select_by_visible_text(domain)
+	time.sleep(0.5)
 
-	# The following link is an example
-	link = "https://archive.sigma2.no/pages/public/datasetDetail.jsf?id="+entry_dictionary[i].get("id")
+#Add field
+if (not field==""):
+	field_menu = Select(driver.find_element_by_id("searchForm:fieldMenu"))
+	field_menu.select_by_visible_text(field)
+	time.sleep(0.5)
 
+#Add subfield
+if (not subfield==""):
+	subfield_menu = Select(driver.find_element_by_id("searchForm:subfieldMenu"))
+	subfield_menu.select_by_visible_text(subfield)
+
+#Add description
+if (not description==""):
+	description_input = driver.find_element_by_name("searchForm:j_idt86")
+	description_input.send_keys(description)
+list_of_ids = []
+
+for category in categories:
 	driver.get(link)
-	#wait = WebDriverWait(driver, 10).until(
-	#       EC.presence_of_element_located((By.ID, "lp_initial"))
-	#    ) # this waits until the page is loaded
-	element_0 = driver.find_element_by_id("lp_initial")
-	# (str(element_0.get_attribute("innerHTML")))
-	td_raw_list = element_0.find_elements_by_tag_name("td")
-	td_list = []
-	for element in td_raw_list:
-		td_list.append(element.get_attribute("innerHTML"))
-	id = link [link.find("=")+1:]
-	td_list[1] = td_list[1][0:td_list[1].find("<button")]
-	title = td_list[1]
-	td_list[5] = td_list[5].replace("Domain: ","")
-	td_list[5] = td_list[5].replace("Field: ","")
-	td_list[5] = td_list[5].replace("Subfield: ","")
-	#td_list[5]:research area (clean) (missing synchronisation with ROHub)
-	#td_list[8]:created_on (clean) (format yyyy-mm-dd)
-	td_list[10] = td_list[10][td_list[10].find("""rf-dt-c">""")+9:td_list[10].find("</td>")]
-	#td_list[10]:creator (clean)
-	#td_list[16]:license (needs parsing) (Ask Esteban)
-	td_list[16] = td_list[16][td_list[16].find(""""h""")+1:td_list[16].find("""" """)]
-	td_list[18] = td_list[18][td_list[18].find(">")+28:td_list[18].find("\n")]
-	#td_list[18]:description
-	description = td_list[18]
-	td_list[20] = td_list[20][td_list[20].find("""rf-dt-c">""")+9:td_list[20].find("</td")]
-	science_publication = td_list[20]
-	td_list[24] = td_list[24][td_list[24].find("""rf-dt-c">""")+9:td_list[24].find("</td")]
-	rights_holder = td_list[24]
-	td_list[28] = td_list[28][td_list[28].find("""rf-dt-c">""")+9:td_list[28].find("</td")]
-	data_manager = td_list[28]
-	td_list[32] = td_list[32][td_list[32].find("""rf-dt-c">""")+9:td_list[32].find("</td")]
-	depositor = td_list[32]
-	citationHTML = element_0.find_elements_by_id ("citationAPA")
-	citation = citationHTML[0].find_elements_by_tag_name("div")[0].get_attribute("innerHTML")
+	time.sleep(1)
+	if (not description==""):
+		description_input = driver.find_element_by_name("searchForm:j_idt86")
+		description_input.clear()
+		description_input.send_keys(description)
+	category_menu = Select(driver.find_element_by_xpath("""//*[@id="searchForm:categoryMenu"]"""))
+	category_menu.select_by_visible_text(category)
+	#excute query
+	search_button = driver.find_element_by_name("searchForm:j_idt318").click()
+	list_per_category = []
+	#scrape list
+	try:
+		content = driver.find_element_by_id("searchresult-section")
+		list_of_content = content.find_elements_by_class_name("rf-edt-c-cnt")
+	except:
+		NoSuchElementException: print ("There is no results for your search. Please modify your enteries and try again")
+		continue
+		
+	for i in range (0, len(list_of_content),5):
+		list_of_content[i] = list_of_content[i].get_attribute("innerHTML")
+		list_of_content[i+2] = list_of_content[i+2].get_attribute("innerHTML")
 
-	RO = {	"id": id, 
-			"type": "Aquí va el tipo. Nos lo traemos desde el motor de busqueda", 
-			"name":title, 
-			"description":description, 
-			"url":link, 
-			"research area":td_list[5], 
-			"created on": td_list[8],
-			"Creator":td_list[10], 
-			"license":td_list[16], 
-			"science publication":science_publication, 
-			"rights holder":rights_holder, 
-			"data manager": data_manager, 
-			"depositor":depositor, 
-			"citation":citation}
-	result_list.append(RO)
+		
+		list_of_content[i] = list_of_content[i][list_of_content[i].find(""";">""")+3:list_of_content[i].find(""";">""")+22]
+		list_of_content[i+2] = list_of_content[i+2][list_of_content[i+2].find(""";">""")+3:list_of_content[i+2].find("""</a>""")]
+		new_ro = {"id":list_of_content[i],"title":list_of_content[i+2]}
+		#print (list_of_content[i])
+		list_per_category.append(new_ro)
+
+	page_counter = 2
+
+	while (1):
+		time.sleep(1)
+
+		try:
+
+			next_page = driver.find_element_by_id ("searchResultForm:j_idt61_ds_"+str(page_counter)).click()
+			time.sleep(1)
+
+			#print("breakpoint 1")
+			content = driver.find_element_by_id("searchresult-section")
+			list_of_content = content.find_elements_by_class_name("rf-edt-c-cnt")
+			#print ("este es "+list_of_content[2].get_attribute("innerHTML"))
+			for i in range (0, len(list_of_content),5):
+				list_of_content[i] = list_of_content[i].get_attribute("innerHTML")
+				list_of_content[i+2] = list_of_content[i+2].get_attribute("innerHTML")
+				
+
+				list_of_content[i] = list_of_content[i][list_of_content[i].find(""";">""")+3:list_of_content[i].find(""";">""")+22]
+				list_of_content[i+2] = list_of_content[i+2][list_of_content[i+2].find(""";">""")+3:list_of_content[i+2].find("""</a>""")]
+				#print (list_of_content[i+2])
+				#print (list_of_content[i])
+				new_ro = {"id":list_of_content[i],"title":list_of_content[i+2]}
+				#print (list_of_content[i])
+				list_per_category.append(new_ro)
+
+			page_counter+=1
+			#print ("este es "+list_of_content[2])
+
+		except:
+			NoSuchElementException:	list_of_ids.append({"Category" : category, "Resources":list_per_category})
+			break
 
 
-#print (RO)
-#app_json = json.dumps(RO, sort_keys=True)
 
-f = open("Massive-ROs-Creator\ROs.json", "w")
-f.write(json.dumps(result_list))
+
+f = open("Massive-ROs-Creator\ToScrape.json", "w")
+f.write(json.dumps(list_of_ids, indent=4, sort_keys=True))
 f.close()
-####################################### This app creates a json file in your working directory ############################################
-
 driver.quit()
+exit()
+
+print("Your querey was excuted correctly and information was saved")
+
+
+
+#list_of_content[i].find("</a")
+#####################ESTA ES UNA PRUEBA PARA SACAR MÁS DATOS DE LA LISTA######################
+#for i in range (0, len(list_of_content)):
+#	list_of_content[i] = list_of_content[i].get_attribute("innerHTML")
+###
+###	list_of_content[i] = list_of_content[i][list_of_content[i].find(""";">""")+3:list_of_content[i].find("</a")]
+###	
+##for i in range (0, len(list_of_content),5)
+#	list	
+
+
+#print (list_of_content[0].get_attribute("innerHTML"))
+#list_of_content = list_of_content.find_elements_by_tag_name("a")
+
+#selection = domain_menu.find_element_by_link_text("Natural sciences")
+#domain_menu = domain_menu.find_element_by_link_text("Not defined")
+
+#domain_menu.send_keys(domain)
+#domain_menu.send_keys(Keys.RETURN)
+#driver.quit()
